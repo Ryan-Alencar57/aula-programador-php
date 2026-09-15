615d7f9d73fc177f6b6906671c1de224560189eb
Antonio Ryan Alencar Reis <alencarryan57@gmail.com>
2026-09-14 21:40:28 -0300

    feat:07-formularios-parte2/19-formularios-post-pratica1/index_processar.php

--- 07-formularios-parte2/19-formularios-post-pratica1/index.php
+++ 07-formularios-parte2/19-formularios-post-pratica1/index.php
@@ -6,8 +6,8 @@ senacClassName("Prática — Cadastro de Conta");
 <?php senacClassSession("Formulário de cadastro", __LINE__); ?>
 
 <form action="index_processar.php" method="POST">
-    <input type="text" name="nome" placeholder="Nome completo"><br>
-    <input type="email" name="email" placeholder="E-mail"><br>
+    <input type="text" name="nome" placeholder="Nome completo" value="Ryan Alencar"><br>
+    <input type="email" name="email" placeholder="E-mail" value="ryan@gmail.com"><br>
     <input type="password" name="senha" placeholder="Senha"><br>
     <input type="password" name="confirmarSenha" placeholder="Confirmar senha"><br>
     <button type="submit">Criar conta</button>
--- 07-formularios-parte2/19-formularios-post-pratica1/index_processar.php
+++ 07-formularios-parte2/19-formularios-post-pratica1/index_processar.php
@@ -12,6 +12,30 @@ $contas = [
 
 // A lógica de cadastro será construída aqui, em aula, junto com a turma.
 
+$nome = $_POST["email"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+$confirmarSenha = $_POST["confirmarSenha"];
+
+if(empty($nome) || empty($email) || empty($senha)  || empty($confirmarSenha)){
+    echo "<p>Informe seus dados corretamente.</p>";
+    die;
+}
+ if($senha !== $confirmarSenha){
+    echo "<p>erro ao cadastrar: as senhas não coincidem.</p>";
+    die;
+ }
+
+ $contas[] = [
+    "nome" => $nome,
+    "email" => $email,
+    "senha" => $senha
+ ];
+
+echo "<p>conta criada com sucesso!</p>";
+
+var_dump($contas);
+
 ?>
 
 <?php
